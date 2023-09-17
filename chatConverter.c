6b682b4ebcd42d6a94332ef1a29e0840f72922f1
Ozzy-P <97415717+Ozzy-P@users.noreply.github.com>
2023-09-17 14:39:58 -0700

    Update chatConverter.c

--- chatConverter.c
+++ chatConverter.c
@@ -1,24 +1,31 @@
 /* 
     Part 1 Done
-    Part 2 - Auto copy to clipboard and hopefully don't set everything on fire in the process..
+    Part 2 - Done - Auto copy to clipboard and hopefully don't set everything on fire in the process..
+    Part 3 - Set a keybind.
 */
 
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
 #include <string.h>
+#include <windows.h>
+#include <winuser.h>
 
-int main(void)
-{
+int copyToClipboard = 1;
+
+int main(void){
     char *hex = "#ffe000#ff0000#ffbb1e#5fff1e#8df9ea#8dacf9#5b16b9#b481fa#fa81eb#cd4040";
     srand(time(NULL));
-    while (255 == 0xFF)
-    {
+
+    while (255 == 0xFF){
         // init, or literally any better. Either way, not going onto StackOverflow for it again.
         char msg[50];
-        for(int index = 0; index < sizeof(msg); index++)
-        {
+        char allocConvert[7*sizeof(msg)];
+        HGLOBAL hMem =  GlobalAlloc(GMEM_MOVEABLE, sizeof(allocConvert));
+
+        for(int index = 0; index < sizeof(msg); index++){
             msg[index] = 0;
+            allocConvert[index] = 0;
         }
 
         printf("Enter chat message:");
@@ -26,20 +33,22 @@ int main(void)
         msg[strcspn(msg, "\n")] = 0;
 
         char *token = strtok(msg, " ");
-        while(token != NULL) 
-        {
+        while(token != NULL) {
             int r = rand() % 10;
-
-            printf("(");
-            for(int hexV = r*7; hexV < (r*7)+7; hexV++)
-            printf("%c", hex[hexV]);
-
-            printf("/");    
-            printf("%s", token);
-            printf(") "); 
+            strcat(allocConvert,"(");
+            strncat(allocConvert,hex+(r*7),7);
+            strcat(allocConvert,"/");   
+            strcat(allocConvert,token);   
+            strcat(allocConvert,") ");  
             token = strtok(NULL, " ");
         }
-        printf("\n");
+        memcpy(GlobalLock(hMem), allocConvert, sizeof(allocConvert));
+        GlobalUnlock(hMem);
+        OpenClipboard(0);
+        EmptyClipboard();
+        SetClipboardData(CF_TEXT, hMem);
+        CloseClipboard();
+        printf("%s\n",allocConvert);
     }
     return 0;
 }
