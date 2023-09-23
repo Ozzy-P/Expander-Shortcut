1152de8ae5d878670e8df7bba71b7e85844ea472
Ozzy-P <97415717+Ozzy-P@users.noreply.github.com>
2023-09-22 17:46:32 -0700

    Update autoConverter.c
    Almost there

--- autoConverter.c
+++ autoConverter.c
@@ -1,9 +1,9 @@
 // Obviously doesn't work, yet, or maybe ever.
 
 /*
-    TODO: *Fix capitalization, somehow. Preferrably just capitalize the first letter then [REDACTED].
-          *Add copy and paste functionality from chatConverter.c
-          *Attempt to auto paste (probably not needed, esp if its a mist).
+    TODO: [W] *Fix capitalization, somehow. Preferrably just capitalize the first letter then [REDACTED].
+          [D] *Add copy and paste functionality from chatConverter.c
+          [D] *Attempt to auto paste (probably not needed, esp if its a mist).
 */
 
 #include <windows.h>
@@ -12,6 +12,7 @@
 int Save(int _key, char *file);
 int SaveInt(int number, char *file);
 const int MAX_CHARS = 50;
+const char *hex = "#ffe000#ff0000#ffbb1e#5fff1e#8df9ea#8dacf9#5b16b9#b481fa#fa81eb#cd4040";
 
 int main(void){
     int keyPressed = 0;
@@ -33,21 +34,26 @@ int main(void){
 
 int Save(int _key, char *file){
     Sleep(10);
-    HGLOBAL hMem =  GlobalAlloc(GMEM_MOVEABLE, 7*50);
+    HGLOBAL hMem =  GlobalAlloc(GMEM_MOVEABLE, 7*MAX_CHARS);
     FILE *OUTPUT_FILE;
     OUTPUT_FILE = fopen(file, "a+");
     fprintf(OUTPUT_FILE, "START\n");
     int activeKey = 1;
     int pressedChars[255+8];
 
-    char allocConvert[50];
+    char typedChars[MAX_CHARS];
+    char allocConvert[MAX_CHARS * 7 + 1];
     int numberChars = 0;
 
     for(int iteration = 0; iteration < 255+8; iteration++){
         pressedChars[iteration] = 0;
     }
 
-    for(int iteration = 0; iteration < 50; iteration++){
+    for(int iteration = 0; iteration < sizeof(typedChars); iteration++){
+        typedChars[iteration] = 0;
+    }
+
+    for(int iteration = 0; iteration < sizeof(allocConvert); iteration++){
         allocConvert[iteration] = 0;
     }
 
@@ -63,6 +69,18 @@ int Save(int _key, char *file){
                 activeKey = 0;
                 fprintf(OUTPUT_FILE, "END\n");
                 fclose(OUTPUT_FILE);
+
+                char *token = strtok(typedChars, " ");
+                while(token != NULL) {
+                    int r = rand() % 10;
+                    strcat(allocConvert,"(");
+                    strncat(allocConvert,hex+(r*7),7);
+                    strcat(allocConvert,"/");   
+                    strcat(allocConvert,token);   
+                    strcat(allocConvert,") ");  
+                    token = strtok(NULL, " ");
+                }
+
                 memcpy(GlobalLock(hMem), allocConvert, sizeof(allocConvert));
                 GlobalUnlock(hMem);
                 OpenClipboard(0);
@@ -73,14 +91,14 @@ int Save(int _key, char *file){
                 break;
             }
             if (!(keyState) && (pressedChars[i] == 1)){
-                allocConvert[numberChars] = i;
-                numberChars++;
                 fprintf(OUTPUT_FILE, "%s", &i);
                 //fprintf(OUTPUT_FILE, "RELEASED:%s,%d\n", &i,i);
+                typedChars[numberChars] = i;
+                numberChars++;
                 pressedChars[i] = 0;
             }
             if ((keyState & 0x8000) && (pressedChars[i] == 0)){
-                fprintf(OUTPUT_FILE, "PRESSED:%s,%d\n", &i,i);
+                //fprintf(OUTPUT_FILE, "PRESSED:%s,%d\n", &i,i);
                 pressedChars[i] = 1;
             }
         }
