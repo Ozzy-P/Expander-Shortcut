c89bec2a4c9d86bdcdff4fb0a215e792f476d546
Ozzy-P <97415717+Ozzy-P@users.noreply.github.com>
2023-09-22 18:31:06 -0700

    Update converter.h
    Mod

--- converter.h
+++ converter.h
@@ -1,1 +1,13 @@
+#include <windows.h>
 
+#ifndef MESSAGE
+#define MESSAGE
+extern const int MAX_CHARS;
+struct message {
+    HGLOBAL hMem;
+    char msg[50];
+};
+char *formatToHex(char *targetArray, char *messageArray, char *hexValues);
+void copyMessage(HGLOBAL hMem, struct message *msgToCopy, int size);
+void clearArray(struct message *msgToClear);
+#endif
